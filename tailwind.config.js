a4d12a5b8922036d4bef8e4bed203d1bf68cbab1
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-10-06 14:56:08 -0600

    Adding new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{'bgImage': "url('/src/assets/wallpaperflare.com_wallpaper.jpg')",},
+      fontFamily:{
+        pixelfont: ['Pixelify Sans']
+      }
+    },
   },
   plugins: [],
 }
