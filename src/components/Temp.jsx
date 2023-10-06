2994c0a45ce5c5b69db2ebffb68475835592c6ff
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-10-06 14:54:49 -0600

    Adding flex, center and font to the function tempLocal

--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -7,14 +7,14 @@ const Temp = () => {
     const [filtro,setFiltro] = useState(1)
 
     useEffect(() => {axios.get('https://api.openweathermap.org/data/2.5/weather?lat=12.13&lon=-86.25&appid=56e1337974ab22108e31121b83bc55f2').then(resp => {
-        console.log("Humedad: ",resp.data.main.humidity)
+        //console.log("Humedad: ",resp.data.main.humidity)
         setVerTemp([resp.data.main])
     })},[filtro])
 
     const tempLocal= () => {
         return (verTemp.map((datos)=>(
-            <div key={datos}>
-                <p>Humidity: {datos.humidity}</p>
+            <div key={datos} className="flex justify-center">
+                <p className="font-pixelfont">Humidity: {datos.humidity}</p>
             </div>
         ))
         )
