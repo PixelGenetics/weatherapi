465adfa3a338f6967c4e7b6c020c68f1bddef70a
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-10-05 21:12:48 -0600

    Changing logic on function nubeStatus

--- src/components/CloudsState.jsx
+++ src/components/CloudsState.jsx
@@ -9,29 +9,27 @@ const CloudsState = () => {
     const [nubes,SetNubes] = useState([])
 
     useEffect(() => {axios.get('https://api.openweathermap.org/data/2.5/weather?lat=12.13&lon=-86.25&appid=56e1337974ab22108e31121b83bc55f2').then(resp => {
-        console.log('resp:',resp.data.weather)
-        SetNubes([resp.data.weather])
+        console.log('resp:',resp.data.weather[0].main)
+        SetNubes([resp.data.weather[0].main])
     })},[filtro])
 
-    const nubeStatus = () => {
-        return (nubes.map((datos,index) => (
-            <div key={datos}>
-                {datos[0].main}
-            </div>
-        ))
-        )
-    }
-    
-    if (nubeStatus === 'Clouds') {
-        <img src={cloudspng} alt="" />
-    }else{
-        <p>Hola</p>
-    }
+    //console.log("nubestatus: ",palabraRetornada)
+
+    //const palabraRetornada = nubes.toString
+
+    function nubeStatus () {
+        let palabra 
+        nubes.map((datos,index) => palabra = datos)
+        return palabra
+    } 
+
+    console.log('adasd',nubeStatus())
 
     return (
         <>
+        
         {
-            nubeStatus() === 'Clouds' ? "Hola" : <img src={cloudspng} alt="" />
+            nubeStatus() === 'Rain' ? <img src={cloudspng} alt="" /> : 'lol'
         }
         </>
     )
