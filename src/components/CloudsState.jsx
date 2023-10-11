d78ac2390b69292a42734fafc5800ad0eec34329
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-10-10 22:56:34 -0600

    .

--- src/components/CloudsState.jsx
+++ src/components/CloudsState.jsx
@@ -13,7 +13,9 @@ const CloudsState = () => {
     const [nubes,SetNubes] = useState([])
 
     useEffect(() => {axios.get('https://api.openweathermap.org/data/2.5/weather?lat=12.13&lon=-86.25&appid=56e1337974ab22108e31121b83bc55f2').then(resp => {
-        //console.log('resp:',resp.data.weather[0].main)
+        
+        console.log('resp:',resp.data.weather[0].main)
+        console.log('resp:',resp.data)
         SetNubes([resp.data.weather[0].main])
     })},[filtro])
 
